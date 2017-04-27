24b733004a109a36c68d00e8b15c7556fc8e89d9
1173860556@qq.com <1173860556@qq.com>
2017-04-28 00:02:46 +0800

    todolist

---
+++ project/my-project/src/components/Hello.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <input v-model="addnew" @keyup.13="addNewItem"/>
+    <ul v-for="item in items">
+      <li v-text="item.things" :class="{finshed:item.isdone,doing:!item.isdone}" @click="toggle(item)"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'todos',
+      items: [
+        {
+          things: 'chifan',
+          isdone: true
+        },
+        {
+          things: 'gongzuo',
+          isdone: false
+        }
+      ],
+      addnew: ''
+    }
+  },
+  methods: {
+    addNewItem () {
+      console.log(this.addnew);
+      this.items.push({
+        things: this.addnew,
+        isdone: false
+      })
+      this.addnew = '';
+    },
+    toggle (item) {
+      item.isdone = !item.isdone;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .finshed{
+    color:red;
+    text-decoration: line-through;
+  }
+  .doing{
+    color:blue;
+  }
+</style>
