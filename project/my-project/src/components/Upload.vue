e7efd2c30db165515dc62acade6ad30d973fea9d
1173860556@qq.com <1173860556@qq.com>
2017-05-04 00:36:06 +0800

    change

--- project/my-project/src/components/Upload.vue
+++ project/my-project/src/components/Upload.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="box">
         <div class="container">
-            <img :src="dataUrl" v-if="hasimg" class="preview-img" @click="previewclick">
-            <div class="cover-center"><p class="cover-p">点击添加图片</p></div>
-            <div class="cover-bottom"><p class="cover-p">点击修改图片</p></div>
+            <img :src="dataUrl" v-if="hasimg" 
+                v-bind:class="['preview-img',{'preview-img-hover':ishover}]" @click="previewclick">
+            <div class="preview-cover" @click="previewclick" @mouseover="addhover" @mouseout="removehover"></div>
         </div>
         <input class="preview-img-input"
             ref="inputer"
@@ -13,7 +13,7 @@
             :accept="accept"
             capture="video"
             @change="handleFileChange"/>
-        <label :for="inputId"></label>
+        <label :for="inputId"><p>点击添加图片</p></label>
         {{errTxt}}{{fileName}}
     </div>
 </template>
@@ -46,11 +46,27 @@
         z-index: 2;
         background: #efeeee;
         border: 1px solid #efeeee;
+        text-align: center;
     }
-        .preview-img:hover, label:hover {
+    label p{
+        font-size: 12px;
+        line-height: 24px;
+        font-weight: 500;
+        margin-top:25px;
+    }
+    .preview-img, label{
+        transition: 0.5s all;
+    }
+        .preview-img:hover, label:hover, .preview-cover:hover {
+            transition: 0.5s all;
+            transform: scale(1.015);
+            box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.3);
+        }
+        .preview-img-hover{
             transition: 0.3s all;
             transform: scale(1.015);
             box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.3);
+            border: 2px solid red;
         }
     .preview-img {
         border-radius: 5px;
@@ -59,28 +75,13 @@
         z-index: 3;
         position: absolute;
     }
-    .cover-center, .cover-bottom{
-        width:100%;
+    .preview-cover{
+        transition: 0.5s all;
+        border-radius: 5px;
+        width: 100%;
+        height: 100%;
         z-index: 4;
-        height: 23px;
-        position:absolute;
-        text-align: center;
-    }
-    .cover-center{
-        top:50%;
-        margin-top:-10px;
-    }
-    .cover-bottom{
-        bottom:0;
-        background: #000;
-        border-bottom-left-radius: 5px;
-        border-bottom-right-radius: 5px;
-    }
-    .cover-center p, .cover-bottom p{
-        font-size: 12px;
-        line-height: 23px;
-        font-weight: 500;
-        color:#ccc;
+        position: absolute;
     }
     .box img {
         border-radius: 5px;
@@ -100,37 +101,44 @@
 	            maxSize: 2000,
 	            // dataUrl: require('../assets/upload.png'),
                 dataUrl: '',
-                fileName: ''
+                fileName: '',
+                ishover: true
         	}
         },
         methods: {
             handleFileChange (e) {
                 if (typeof e.target === 'undefined') this.file = e[0];
-                else this.file = e.target.files[0];
-                let size = Math.floor(this.file.size / 1024);
+                else this.file = e.target.files[0]
+                let size = Math.floor(this.file.size / 1024)
                 if (size > this.maxSize) {
-                    this.errTxt = '文件最大不能超过2M';
+                    this.errTxt = '文件最大不能超过2M'
                 }
-                let accept = this.accept.split(',');
-                this.onChange && this.onChange(this.file, e.target.value);
-                this.imgPreview(this.file);
-                this.fileName = this.file.name;
-                this.hasimg = true;
+                let accept = this.accept.split(',')
+                this.onChange && this.onChange(this.file, e.target.value)
+                this.imgPreview(this.file)
+                this.fileName = this.file.name
+                this.hasimg = true
             },
             previewclick () {
                 document.getElementById(this.inputId).click();
             },
             imgPreview (file) {
-                let self = this;
-                if (!file || !window.FileReader) return;
+                let self = this
+                if (!file || !window.FileReader) return
                 if (/^image/.test(file.type)) {
-                    var reader = new FileReader();
-                    reader.readAsDataURL(file);
+                    var reader = new FileReader()
+                    reader.readAsDataURL(file)
 
                     reader.onloadend = function () {
-                        self.dataUrl = this.result;
+                        self.dataUrl = this.result
                     }
                 }
+            },
+            addhover () {
+                this.ishover = ture
+            },
+            removehover () {
+                this.ishover = false
             }
         }
     }
